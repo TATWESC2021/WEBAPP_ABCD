4f1b5788c15b7eacddd88119cd91618b5f552918
carlos <carlosdaniel251099@gmail.com>
2021-04-04 13:40:40 -0500

    cambio

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
+
