a9c9f1d2699d53e5521cefedf3047b5f80b0beba
carlos <carlosdaniel251099@gmail.com>
2021-04-04 16:09:38 -0500

    vdhyt

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
-document.addEventListener('DOMContentLoaded',()=> {
-	const elementosCarousel=document.querySelectorAll('.carousel');
-	M.Carousel.init(elementosCarousel,{
-		duration:150
-	});
-});
