f78b25eab5fa21ac067f77b8d228ec9117df8bae
carlos <carlosdaniel251099@gmail.com>
2021-04-12 18:04:49 -0500

    cambio

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
+  $("#btn_registrar").click(function(){
+        $("#modal_custom").find(".modal-header").css("background", "linear-gradient(to left, #5B6FBD, #112B94)");
+        $("#modal_custom").find(".modal-header").css("color", "white");
+        $("#modal_custom").find(".modal-title").text("Ingresa aquí tus datos para crear un usuario");    
+        $('#modal_custom').modal('show');
+    });
